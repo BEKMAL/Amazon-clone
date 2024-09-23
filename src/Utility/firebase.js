fd3b96f058ee53746e441dd9a613a245fe349556
BEKMAL <bekalumsc@gmail.com>
2024-09-23 15:21:34 +0300

    modifiying firebase

--- src/Utility/firebase.js
+++ src/Utility/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";  
+import firebase from "firebase/compat/app";
+import { getAuth } from "firebase/auth";
 import "firebase/compat/firestore";
 import "firebase/compat/auth";
 
@@ -15,6 +15,6 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = app.firestore();
