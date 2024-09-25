501f95fe5497cc863ea3faf22cfdaee092e219fc
BEKMAL <bekalumsc@gmail.com>
2024-09-25 09:15:15 +0300

    update protected route

--- src/Components/ProtectedRoute/ProtectedRoute.jsx
+++ src/Components/ProtectedRoute/ProtectedRoute.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { DataContext } from "../DataProvider/DataProvider";
 
+
 const ProtectedRoute = ({ children, msg, redirect }) => {
   const navigate = useNavigate();
   const [{ user }, dispatch] = useContext(DataContext);
