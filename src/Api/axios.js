0edf2a7f2ae747664c38f8cf4f0267dc90d2bc99
BEKMAL <bekalumsc@gmail.com>
2024-09-25 19:08:35 +0300

    base url addede for render

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -2,9 +2,11 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // local instance of firebase functions
-  baseURL: "http://127.0.0.1:5001/clone-4681f/us-central1/api",
-
-
+  // baseURL: "http://127.0.0.1:5001/clone-4681f/us-central1/api",
+  //localhost
+// baseURL: "http://127.0.0.1:5001",
+  //deploy on render.com
+  baseURL: "https://amazon-api-z8q9.onrender.com",
 });
 
 export { axiosInstance };
