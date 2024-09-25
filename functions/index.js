0843740343ee8e11086575687cfa26509760d53d
BEKMAL <bekalumsc@gmail.com>
2024-09-25 09:00:53 +0300

    update function index.js

--- functions/index.js
+++ functions/index.js
@@ -34,5 +34,5 @@ app.get("/", (req, res) => {
       });
     }
   });
-// app.listen(4050, console.log("Amazon Server Running ..."));
+// app.listen(4050, console.log("Amazon Server Running ...."));
 exports.api = onRequest(app);
